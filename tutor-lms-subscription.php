7e4157701a12689b5be26aebe15f25d2536ff050
ok9xnirab <istiaqanirab202@gmail.com>
2021-06-13 17:05:44 +0600

    module ready for plugin use

--- tutor-lms-subscription.php
+++ tutor-lms-subscription.php
@@ -1,4 +1,42 @@
 <?php
+/*
+Plugin Name: Tutor LMS Subscription
+Plugin URI: https://wordpress.org/plugins/tutor-lms-subscription
+Description: Tutor LMS support to our subscription module
+Version: 1.0.0
+Author: SpringDevs
+Author URI: https://springdevs.com/
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: sdevs_tls
+Domain Path: /languages
+*/
+
+/**
+ * Copyright (c) 2021 SpringDevs (email: contact@springdevs.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
 
 // don't call the file directly
 if (!defined('ABSPATH')) {
@@ -142,6 +180,9 @@ final class Tutor_subscrpt
     public function init_hooks()
     {
         add_action('init', [$this, 'init_classes']);
+
+        // Localize our plugin
+        add_action('init', [$this, 'localization_setup']);
     }
 
     /**
@@ -157,6 +198,16 @@ final class Tutor_subscrpt
 
         $this->container['api']    = new Springdevs\TutorSubscrpt\Api();
         $this->container['assets'] = new Springdevs\TutorSubscrpt\Assets();
+    }
+
+    /**
+     * Initialize plugin for localization
+     *
+     * @uses load_plugin_textdomain()
+     */
+    public function localization_setup()
+    {
+        load_plugin_textdomain('sdevs_tls', false, dirname(plugin_basename(__FILE__)) . '/languages/');
     }
 
     /**
